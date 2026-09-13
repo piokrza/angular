a2682a12c8e755ee46c87034c4dd6bb671241c14
Piotr Krzaczkowski <slimhannya@gmail.com>
2026-09-13 09:47:29 +0200

    feat: add husky, lint-staged

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+
+import { TestBed } from '@angular/core/testing';
 
 describe('App', () => {
   beforeEach(async () => {
